8aa2e57e41bb622a25c569a8aa7e0557525edd17
Egor-bog <7548494@gmail.com>
2021-02-01 11:36:32 +0300

    fix: index.js

--- src/index.js
+++ src/index.js
@@ -1,12 +1,22 @@
 
 exports.min = function min (array) {
-  return 0;
-}
+  if (array == undefined || array.length == 0) {return 0;}
+  let min = Math.min.apply(null,array);
+  return min;
+}  
 
 exports.max = function max (array) {
-  return 0;
+  if (array == undefined || array.length == 0) {return 0;}
+  let max = Math.max.apply(null,array);
+  return max;
 }
 
 exports.avg = function avg (array) {
-  return 0;
+  if (array == undefined || array.length == 0) {return 0;}
+  let sum = 0;
+  for (i = 0; i < array.length; i++) {
+    sum += array[i];
+  }
+  let avg = sum / array.length;
+  return avg;
 }
